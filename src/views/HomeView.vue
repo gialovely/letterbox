9ba125514b86eb912c0139ba08c1a6e7d31633ba
gialovely <gialovely96@proton.me>
2025-02-10 22:25:17 +0100

    setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const x = ref(0);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Hello World {{ x * 2 }}
+    <br />
+    <button @click="x += 1">Increase</button>
   </main>
 </template>
